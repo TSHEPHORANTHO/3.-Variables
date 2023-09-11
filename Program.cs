d34a55646cacf5e02a0e73bb534097bd98102186
Dell-User <Dell-User@DESKTOP-88R8045>
2023-09-11 09:42:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaration
+            string name;
+            int age;
+            //initiallizing
+            name = "Budha";
+            age = 10;
+            //printing
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+
+            //freeze
+            Console.ReadLine();
+        }
+    }
+}
